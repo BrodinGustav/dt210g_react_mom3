6ab0e0ee5d07534a46c4d3017236dece3d91f83e
Gustav Brodin <brodin.gustav@gmail.com>
2025-05-14 16:05:20 +0200

    nytt test för o se om CRUD stämmre

--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -35,9 +35,9 @@ export const AuthProvider: React.FC<AuthProviderProps> = ({ children }) => {    
 
     
         try {
-        //    const res = await fetch("https://dt210g-mom3-backend-1.onrender.com/api/auth/login/", {  //Om ok inlogg genereras token som skickas till klienten ÄNDRA TILLBAKA 
+            const res = await fetch("https://dt210g-mom3-backend-1.onrender.com/api/auth/login/", {  //Om ok inlogg genereras token som skickas till klienten  
 
-            const res = await fetch("http://localhost:5000/api/auth/login", {  //Om ok inlogg genereras token som skickas till klienten
+           // const res = await fetch("http://localhost:5000/api/auth/login", {  //Om ok inlogg genereras token som skickas till klienten
                 method: "POST",
                 headers: {
                     "Content-Type": "application/json"
